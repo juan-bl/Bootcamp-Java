e29e7882180b825a644122be695c5fb5be2e4366
juan-bl <juannicolasdev@gmail.com>
2024-07-07 16:17:14 -0300

    Aula1-tipos-e-variaveis

---
+++ aula1/2-tipos-e-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,20 @@
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        String meuNome = "Juan Nícolas";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double valor_de_pi = 3.14;
+
+    }
+}
